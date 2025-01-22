1a87133e74cf641217ea5a5960da3ce1861cf78a
Luis Crespo <lcrespom@gmail.com>
2025-01-22 21:56:54 +0100

    Full list of slides

--- src/slides.ts
+++ src/slides.ts
@@ -1,6 +1,6 @@
 const slides: Array<number | Array<number>> = [
   104, // Introduction
-  106, // Divider
+  106, // ----- Divider -----
   [0, 27], // Browser
   [6, 28], // Server
   [35, 37], // Native
@@ -8,8 +8,17 @@ const slides: Array<number | Array<number>> = [
   50, // Electron App bundle
   52, // Tauri
   [67, 77], // Tauri App bundle
-  86, // Divider,
-  [88, 89] // Rust
+  86, // ----- Divider, -----
+  [88, 89], // Rust
+  [90, 91], // Solution
+  105, // ----- Divider -----
+  108, // Terminal limitations
+  109, // Approach
+  124, // Using HTTP
+  [129, 131], // Main features
+  [136, 138], // Customization
+  139, // ----- Divider -----
+  [140, 141] // Thank you
 ]
 
 export default slides
